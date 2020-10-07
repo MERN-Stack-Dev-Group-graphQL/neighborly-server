642b16f6a52e7c9a7767eecf29ad9e89e8d3b8b4
Jessica Stenning <jessica.stenning@engineerbetter.com>
2020-10-07 16:09:42 +0100

    create ci directory with terraform files

---
+++ ci/terraform/ec2.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "eu-west-1"
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated-key" {
+  key_name = "neighborly-server-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "aws_instance" "neighborly-server" {
+  ami           = "ami-0bb3fad3c0286ebd5"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.generated-key.key_name
+}
