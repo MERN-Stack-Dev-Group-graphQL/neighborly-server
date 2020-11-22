a94bda0cb401b4673dde786bbe6c641092f3e3b6
JizongL <2015rpro@gmail.com>
2020-11-22 11:11:06 -0500

    added security group in terraform for instance

--- ci/terraform/ec2.tf
+++ ci/terraform/ec2.tf
@@ -15,7 +15,7 @@ provider "aws" {
 resource "aws_instance" "neighborly_server_dev" {
   ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t2.micro"
-  security_groups = [ "sg-0badf30d69e4dca5d" ]
+  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
   key_name = "neighborly-server-test"
   tags = {
     Name = "neighborly_server_dev"
