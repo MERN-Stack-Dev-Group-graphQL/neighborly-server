e0a7a1b3167175c4720251f454cb94259a6c342e
JizongL <2015rpro@gmail.com>
2020-11-22 21:15:56 -0500

    updated script for start

--- ci/start.sh
+++ ci/start.sh
@@ -1,12 +1,11 @@
 
-tar xzf /home/ec2-user/neighborly-server.tar.gz -C /var/www/neighborly.tools/neighborly-server
 sudo yum update -y
-sudo amazon-linux-extras install nginx -y
+sudo yum install nginx -y
 sudo systemctl start nginx
-sudo cp /var/www/neighborly.tools/neighborly-server/nginx/neighborly.tools.conf /etc/nginx/conf.d/neighborly.tools.conf
+sudo cp /var/www/neighborly.tools/nginx/neighborly.tools.conf /etc/nginx/conf.d/neighborly.tools.conf
 cd /tmp
 wget -O epel.rpm –nv https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y ./epel.rpm
-sudo yum install python2-certbot.noarch
-sudo yum install python-certbot-nginx
-DEBIAN_FRONTEND=noninteractive sudo certbot --nginx
+DEBIAN_FRONTEND=noninteractive sudo yum install -y ./epel.rpm
+DEBIAN_FRONTEND=noninteractive sudo yum install python2-certbot.noarch -y
+DEBIAN_FRONTEND=noninteractive sudo yum install python-certbot-nginx -y
+DEBIAN_FRONTEND=noninteractive sudo certbot -n --nginx --email 2015rpro@gmail.com --agree-tos
